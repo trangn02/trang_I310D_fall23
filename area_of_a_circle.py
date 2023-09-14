42a51e1f769001075cef6f69757ffb8682528fc7
Trang Nguyen <trangnguyen@wireless-10-146-103-131.public.utexas.edu>
2023-09-14 16:21:47 -0500

    Added a comment, changed a variable

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,9 +1,10 @@
+# This program computes the area of a circle
 def compute_area_of_circle(radius):
 	pi = 3.14
 	area = pi * radius * radius
 	return area
 
-radius1 = 30
+radius0 = 30
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
